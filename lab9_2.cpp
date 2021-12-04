0353c8e54abd7cb1775f8b58d6975451874eaf74
Sakuraii <pearl.k.sakura@gmail.com>
2021-12-04 23:26:12 +0700

    read me change , lab01 and lab02 finished

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -4,6 +4,21 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double grade){
+	if(grade >90){
+		return 'A';
+	}else if(grade >75){
+		return 'B';
+	}else if(grade>60){
+		return 'C';
+	}else if(grade>45){
+		return 'D';
+	}else if(grade>=0){
+		return 'F';
+	}else{
+		return 'E';
+	}
+}
 
 int main(){
 	//Input the number of students
@@ -17,9 +32,11 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
+		getline(cin,name[i]);
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
